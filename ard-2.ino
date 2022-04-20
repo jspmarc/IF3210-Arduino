bf8aba176b819a6d8bb551ec97410f7d609faadf
Josep Marcello <jspmarcello@live.com>
2022-04-20 11:23:28 +0700

    feat: enter and exit sensor

--- ard-2.ino
+++ ard-2.ino
@@ -1,25 +1,36 @@
 // C++ code
 // 2nd arduino (1st slave)
 
-#define PIR_PIN 12
-#define IR_PIN 7
+#define PIR_ENTER_PIN 12
+#define PIR_OUT_PIN 11
+#define BUTTON_PIN 7
 
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
-	pinMode(PIR_PIN, INPUT);
+	pinMode(PIR_ENTER_PIN, INPUT);
+	pinMode(PIR_OUT_PIN, INPUT);
+	pinMode(BUTTON_PIN, INPUT);
 	Serial.begin(9600);
 }
 
+unsigned short people_counter = 0;
+
 void loop()
 {
-	int pir_reading = digitalRead(PIR_PIN);
-	if (pir_reading == HIGH)
-		digitalWrite(LED_BUILTIN, HIGH);
-	else
-		digitalWrite(LED_BUILTIN, LOW);
+	byte pir_enter_reading = digitalRead(PIR_ENTER_PIN);
+	byte pir_out_reading = digitalRead(PIR_OUT_PIN);
 
-	int ir_reading = digitalRead(IR_PIN);
+	if (pir_enter_reading == HIGH && pir_out_reading == LOW) {
+		people_counter++;
+		delay(1500);
+    }
+	if (pir_enter_reading == LOW && pir_out_reading == HIGH) {
+		people_counter = people_counter > 0 ? people_counter - 1 : 0;
+		delay(1500);
+    }
+	digitalWrite(LED_BUILTIN, people_counter > 0);
+	
 
 	delay(100);
 }
